3fe4d0816f6a1feb53ed7fdd221431c635840518
irfan <irfan.roslan.21@ucl.ac.uk>
2024-02-23 16:35:15 +0000

    Play around with HTML and CSS

--- src/paralympics_dash/paralympics_app.py
+++ src/paralympics_dash/paralympics_app.py
@@ -1,9 +1,17 @@
 from dash import Dash, html
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+external_stylesheets = [dbc.themes.VAPOR]
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    html.Div(children='Hello World')
+    html.H1('Hello World'),
+    html.Div([
+        html.P('This is a simple Dash web application.'),
+        html.P('It is a single page with a title and a paragraph.'),
+        html.Img(src=app.get_asset_url('bar-chart-placeholder.png'))
+    ])
 ])
 
 if __name__ == '__main__':
