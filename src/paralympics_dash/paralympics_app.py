86cde2ab2286af2057f0791b5e5133540e8caedb
irfan <irfan.roslan.21@ucl.ac.uk>
2024-02-23 18:54:51 +0000

    Finish week 1 activities

--- src/paralympics_dash/paralympics_app.py
+++ src/paralympics_dash/paralympics_app.py
@@ -9,13 +9,86 @@ meta_tags = [
 
 app = Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=meta_tags)
 
-app.layout = dbc.Container([
-    html.H1('Hello World'),
-    html.Div([
-        html.P('This is a simple Dash web application.'),
-        html.P('It is a single page with a title and a paragraph.'),
-        html.Img(src=app.get_asset_url('bar-chart-placeholder.png'), style={'width': '100%'})
+row_one = dbc.Row([
+    dbc.Col([
+        html.H1(
+            "Paralympics Data Analytics"
+        ),
+        html.P(
+            "Lorem ipsum dolor sit amet."
+        )
     ])
+])
+
+row_two = dbc.Row([
+    dbc.Col([dbc.Select(
+    options=[
+        {"label": "Events", "value": "events"},  # The value is in the format of the column heading in the data
+        {"label": "Sports", "value": "sports"},
+        {"label": "Countries", "value": "countries"},
+        {"label": "Athletes", "value": "participants"},
+    ],
+    value="events",  # The default selection
+    id="dropdown-input",  # id uniquely identifies the element, will be needed later for callbacks
+)], width=2),
+    dbc.Col([html.Img(src=app.get_asset_url('line-chart-placeholder.png'), className="img-fluid")], width=4),
+    dbc.Col([html.Div(
+    [
+        dbc.Label("Select the Paralympic Games type"),
+        dbc.Checklist(
+            options=[
+                {"label": "Summer", "value": "summer"},
+                {"label": "Winter", "value": "winter"},
+            ],
+            value=["summer"],  # Values is a list as you can select 1 AND 2
+            id="checklist-input",
+        ),
+    ]
+)], width=2),
+    dbc.Col([html.Img(src=app.get_asset_url('bar-chart-placeholder.png'), className="img-fluid")], width=4),
+])
+
+row_three = dbc.Row([
+    dbc.Col([html.Img(src=app.get_asset_url('map-placeholder.png'), className="img-fluid")], width=6),
+    dbc.Col([dbc.Card(
+    [
+        dbc.CardImg(src=app.get_asset_url("logos/2022_Beijing.jpg")),
+        dbc.CardBody(
+            [
+                html.H4("TownName 2026", className="card-title"),
+                html.P(
+                    "Highlights of the paralympic event will go here. This will be a sentence or two.",
+                    className="card-text",
+                ),
+                html.P(
+                    "Number of athletes: XX",
+                    className="card-text",
+                ),
+                html.P(
+                    "Number of events: XX",
+                    className="card-text",
+                ),
+                html.P(
+                    "Number of countries: XX",
+                    className="card-text",
+                ),
+            ]
+        ),
+    ],
+    # style={"width": "18rem"},
+)], width=6),
+])
+
+row_four = dbc.Row([
+    dbc.Col([], width=6),
+    dbc.Col([], width=6),
+])
+
+app.layout = dbc.Container([
+    row_one,
+    row_two,
+    row_three,
+    row_four
 ])
 
 if __name__ == '__main__':
