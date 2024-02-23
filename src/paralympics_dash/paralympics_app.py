40affc3b28e820f889e9c7c608f9d2694dac34ed
irfan <irfan.roslan.21@ucl.ac.uk>
2024-02-23 16:44:02 +0000

    Update app to responsive design

--- src/paralympics_dash/paralympics_app.py
+++ src/paralympics_dash/paralympics_app.py
@@ -3,14 +3,18 @@ import dash_bootstrap_components as dbc
 
 external_stylesheets = [dbc.themes.VAPOR]
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+meta_tags = [
+    {"name": "viewport", "content": "width=device-width, initial-scale=1"}
+]
 
-app.layout = html.Div([
+app = Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=meta_tags)
+
+app.layout = dbc.Container([
     html.H1('Hello World'),
     html.Div([
         html.P('This is a simple Dash web application.'),
         html.P('It is a single page with a title and a paragraph.'),
-        html.Img(src=app.get_asset_url('bar-chart-placeholder.png'))
+        html.Img(src=app.get_asset_url('bar-chart-placeholder.png'), style={'width': '100%'})
     ])
 ])
 
